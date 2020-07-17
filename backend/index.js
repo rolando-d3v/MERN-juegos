2942a56a65766b1ea4b6abd2b4660a2500362575
raidentek <33987212+raidentek@users.noreply.github.com>
2020-07-16 20:34:22 -0500

    ades

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+// importat la librerias
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+// use metodos de libs
+const app = express()
+require()
+
+
+// middlewares
